ae17b9b87f78e7c23f458b4c2bae02872269188e
Florent Kermarrec <florent@enjoy-digital.fr>
2020-01-15 23:21:19 +0100

    targets: add 1gbps ethernet and 32-bit DDR3 pins, remove GTP test features

--- ac701.py
+++ ac701.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# This file is Copyright (c) 2017-2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# This file is Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
 # License: BSD
 
 import sys
@@ -11,8 +11,15 @@ from litex.build.generic_platform import *
 from litex.build.xilinx import XilinxPlatform
 
 from litex.soc.cores.clock import *
-from litex.soc.integration.soc_core import *
+from litex.soc.integration.soc_sdram import *
 from litex.soc.integration.builder import *
+
+from litedram.modules import MT8JTF12864
+from litedram.phy import s7ddrphy
+
+from liteeth.phy.s7rgmii import LiteEthPHYRGMII
+from liteeth.core import LiteEthUDPIPCore
+from liteeth.frontend.etherbone import LiteEthEtherbone
 
 from liteiclink.transceiver.gtp_7series import GTPQuadPLL, GTP
 
@@ -53,8 +60,54 @@ _io = [
         Subsignal("p", Pins("B13")),
         Subsignal("n", Pins("AA3"))
     ),
-]
 
+    ("eth_clocks", 0,
+        Subsignal("tx", Pins("U22")),
+        Subsignal("rx", Pins("U21")),
+        IOStandard("LVCMOS18")
+    ),
+    ("eth", 0,
+        Subsignal("rx_ctl", Pins("U14")),
+        Subsignal("rx_data", Pins("U17 V17 V16 V14")),
+        Subsignal("tx_ctl", Pins("T15")),
+        Subsignal("tx_data", Pins("U16 U15 T18 T17")),
+        Subsignal("rst_n", Pins("V18")),
+        Subsignal("mdc", Pins("W18")),
+        Subsignal("mdio", Pins("T14")),
+        IOStandard("LVCMOS18"), Misc("SLEW=FAST"), Drive(16)
+    ),
+
+    ("ddram", 0,
+        Subsignal("a", Pins(
+            "M4 J3 J1 L4 K5 M7 K1 M6",
+            "H1 K3 N7 L5 L7 N6 L3 K2"),
+            IOStandard("SSTL15")),
+        Subsignal("ba", Pins("N1 M1 H2"), IOStandard("SSTL15")),
+        Subsignal("ras_n", Pins("P1"), IOStandard("SSTL15")),
+        Subsignal("cas_n", Pins("T4"), IOStandard("SSTL15")),
+        Subsignal("we_n", Pins("R1"), IOStandard("SSTL15")),
+        Subsignal("cs_n", Pins("T3"), IOStandard("SSTL15")),
+        Subsignal("dm", Pins("AC6 AC4 AA3 U7"),
+            IOStandard("SSTL15")),
+        Subsignal("dq", Pins(
+            "AB6 AA8 Y8 AB5 AA5 Y5 Y6 Y7",
+            "AF4 AF5 AF3 AE3 AD3 AC3 AB4 AA4",
+            "AC2 AB2 AF2 AE2 Y1 Y2 AC1 AB1",
+            "Y3 W3 W6 V6 W4 W5 W1 V1"),
+            IOStandard("SSTL15"),
+            Misc("IN_TERM=UNTUNED_SPLIT_50")),
+        Subsignal("dqs_p", Pins("V8 AD5 AD1 V3"),
+            IOStandard("DIFF_SSTL15")),
+        Subsignal("dqs_n", Pins("W8 AE5 AE1 V2"),
+            IOStandard("DIFF_SSTL15")),
+        Subsignal("clk_p", Pins("M2"), IOStandard("DIFF_SSTL15")),
+        Subsignal("clk_n", Pins("L2"), IOStandard("DIFF_SSTL15")),
+        Subsignal("cke", Pins("P4"), IOStandard("SSTL15")),
+        Subsignal("odt", Pins("R2"), IOStandard("SSTL15")),
+        Subsignal("reset_n", Pins("N8"), IOStandard("LVCMOS15")),
+        Misc("SLEW=FAST"),
+    ),
+]
 
 # Platform -----------------------------------------------------------------------------------------
 
@@ -66,8 +119,10 @@ class Platform(XilinxPlatform):
 
 class _CRG(Module):
     def __init__(self, platform, sys_clk_freq):
-        self.clock_domains.cd_sys    = ClockDomain()
-        self.clock_domains.cd_clk125 = ClockDomain()
+        self.clock_domains.cd_sys       = ClockDomain()
+        self.clock_domains.cd_sys4x     = ClockDomain(reset_less=True)
+        self.clock_domains.cd_sys4x_dqs = ClockDomain(reset_less=True)
+        self.clock_domains.cd_clk200    = ClockDomain()
 
         # # #
 
@@ -75,48 +130,87 @@ class _CRG(Module):
         rst    = platform.request("cpu_reset")
         platform.add_period_constraint(clk200.p, 1e9/200e6)
 
-        self.submodules.pll = pll = S7PLL()
+        self.submodules.pll = pll = S7MMCM(speedgrade=-2)
         self.comb += pll.reset.eq(rst)
         pll.register_clkin(clk200, 200e6)
-        pll.create_clkout(self.cd_sys, sys_clk_freq)
-        pll.create_clkout(self.cd_clk125, 125e6)
+        pll.create_clkout(self.cd_sys,       sys_clk_freq)
+        pll.create_clkout(self.cd_sys4x,     4*sys_clk_freq)
+        pll.create_clkout(self.cd_sys4x_dqs, 4*sys_clk_freq, phase=90)
+        pll.create_clkout(self.cd_clk200,    200e6)
 
+        self.submodules.idelayctrl = S7IDELAYCTRL(self.cd_clk200)
 
-class GTPTestSoC(SoCMini):
-    def __init__(self, platform, connector="pcie", linerate=2.5e9, use_pcie_refclk=True, with_loopback=True):
+# PCIe Analyzer ------------------------------------------------------------------------------------
+
+class PCIeAnalyzer(SoCSDRAM):
+    def __init__(self, platform, connector="pcie", linerate=2.5e9):
         assert connector in ["pcie"]
-        sys_clk_freq = int(100e6)
+        sys_clk_freq = int(150e6)
 
-        # SoCMini ----------------------------------------------------------------------------------
-        SoCMini.__init__(self, platform, sys_clk_freq)
+        # SoCSDRAM ----------------------------------------------------------------------------------
+        SoCSDRAM.__init__(self, platform, sys_clk_freq,
+            integrated_rom_size  = 0x8000,
+            integrated_sram_size = 0x1000,
+            uart_name            = "crossover",
+            csr_data_width       = 32,
+        )
 
         # CRG --------------------------------------------------------------------------------------
         self.submodules.crg = _CRG(platform, sys_clk_freq)
         platform.add_period_constraint(self.crg.cd_sys.clk, 1e9/100e6)
 
-        # GTP RefClk -------------------------------------------------------------------------------
-        if use_pcie_refclk:
-            refclk      = Signal()
-            refclk_freq = 100e6
-            refclk_pads = platform.request("pcie_refclk")
-            self.specials += Instance("IBUFDS_GTE2",
-                i_CEB   = 0,
-                i_I     = refclk_pads.p,
-                i_IB    = refclk_pads.n,
-                o_O     = refclk)
-        else:
-            refclk      = Signal()
-            refclk_freq = 125e6
-            self.comb += refclk.eq(ClockSignal("clk125"))
-            platform.add_platform_command("set_property SEVERITY {{Warning}} [get_drc_checks REQP-49]")
+        # DDR3 SDRAM -------------------------------------------------------------------------------
+        self.submodules.ddrphy = s7ddrphy.A7DDRPHY(platform.request("ddram"),
+            memtype      = "DDR3",
+            nphases      = 4,
+            sys_clk_freq = sys_clk_freq)
+        self.add_csr("ddrphy")
+        sdram_module = MT8JTF12864(sys_clk_freq, "1:4")
+        self.register_sdram(self.ddrphy,
+            geom_settings   = sdram_module.geom_settings,
+            timing_settings = sdram_module.timing_settings)
+
+        # Ethernet ---------------------------------------------------------------------------------
+        # phy
+        self.submodules.eth_phy = LiteEthPHYRGMII(
+            clock_pads = platform.request("eth_clocks"),
+            pads       = platform.request("eth"))
+        self.add_csr("eth_phy")
+        # core
+        self.submodules.eth_core = LiteEthUDPIPCore(
+            phy         = self.eth_phy,
+            mac_address = 0x10e2d5000000,
+            ip_address  = "192.168.1.50",
+            clk_freq    = sys_clk_freq)
+        # etherbone
+        self.submodules.etherbone = LiteEthEtherbone(self.eth_core.udp, 1234)
+        self.add_wb_master(self.etherbone.wishbone.bus)
+
+        # timing constraints
+        self.platform.add_period_constraint(self.eth_phy.crg.cd_eth_rx.clk, 1e9/125e6)
+        self.platform.add_period_constraint(self.eth_phy.crg.cd_eth_tx.clk, 1e9/125e6)
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.eth_phy.crg.cd_eth_rx.clk,
+            self.eth_phy.crg.cd_eth_tx.clk)
+
+        # GTX RefClk -------------------------------------------------------------------------------
+        refclk      = Signal()
+        refclk_freq = 100e6
+        refclk_pads = platform.request("pcie_refclk")
+        self.specials += Instance("IBUFDS_GTE2",
+            i_CEB   = 0,
+            i_I     = refclk_pads.p,
+            i_IB    = refclk_pads.n,
+            o_O     = refclk)
 
         # GTP PLL ----------------------------------------------------------------------------------
         qpll = GTPQuadPLL(refclk, refclk_freq, linerate)
         print(qpll)
         self.submodules += qpll
 
+        # GTPs -------------------------------------------------------------------------------------
         for i in range(2):
-            # GTP --------------------------------------------------------------------------------------
             tx_pads = platform.request(connector + "_tx", i)
             rx_pads = platform.request(connector + "_rx", i)
             gtp = GTP(qpll, tx_pads, rx_pads, sys_clk_freq,
@@ -125,31 +219,12 @@ class GTPTestSoC(SoCMini):
                 tx_buffer_enable = True,
                 rx_buffer_enable = True)
             setattr(self.submodules, "gtp"+str(i), gtp)
-            if with_loopback:
-                self.comb += gtp.loopback.eq(0b010) # Near-End PMA Loopback
             platform.add_period_constraint(gtp.cd_tx.clk, 1e9/gtp.tx_clk_freq)
             platform.add_period_constraint(gtp.cd_rx.clk, 1e9/gtp.rx_clk_freq)
             self.platform.add_false_path_constraints(
                 self.crg.cd_sys.clk,
                 gtp.cd_tx.clk,
                 gtp.cd_rx.clk)
-
-            # Test -------------------------------------------------------------------------------------
-            counter = Signal(32)
-            sync_tx = getattr(self.sync, "gtp{}_tx".format(i))
-            sync_tx += counter.eq(counter + 1)
-
-            # K28.5 and slow counter --> TX
-            self.comb += [
-                gtp.encoder.k[0].eq(1),
-                gtp.encoder.d[0].eq((5 << 5) | 28),
-                gtp.encoder.k[1].eq(0),
-                gtp.encoder.d[1].eq(counter[26:]),
-            ]
-
-           # RX (slow counter) --> Leds
-            for j in range(2):
-                self.comb += platform.request("user_led", 2*i+j).eq(gtp.decoders[1].d[i])
 
 # Load ---------------------------------------------------------------------------------------------
 
@@ -165,7 +240,7 @@ def main():
     if "load" in sys.argv[1:]:
         load()
     platform = Platform()
-    soc     = GTPTestSoC(platform)
+    soc     = PCIeAnalyzer(platform)
     builder = Builder(soc, output_dir="build")
     builder.build(build_name="ac701")
 
--- kc705.py
+++ kc705.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# This file is Copyright (c) 2017-2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# This file is Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
 # License: BSD
 
 import sys
@@ -11,8 +11,15 @@ from litex.build.generic_platform import *
 from litex.build.xilinx import XilinxPlatform
 
 from litex.soc.cores.clock import *
-from litex.soc.integration.soc_core import *
+from litex.soc.integration.soc_sdram import *
 from litex.soc.integration.builder import *
+
+from litedram.modules import MT8JTF12864
+from litedram.phy import s7ddrphy
+
+from liteeth.phy import LiteEthPHY
+from liteeth.core import LiteEthUDPIPCore
+from liteeth.frontend.etherbone import LiteEthEtherbone
 
 from liteiclink.transceiver.gtx_7series import GTXChannelPLL, GTX
 
@@ -57,8 +64,60 @@ _io = [
         Subsignal("p", Pins("P6")),
         Subsignal("n", Pins("P5"))
     ),
-]
 
+    ("eth_clocks", 0,
+        Subsignal("tx", Pins("M28")),
+        Subsignal("gtx", Pins("K30")),
+        Subsignal("rx", Pins("U27")),
+        IOStandard("LVCMOS25")
+    ),
+    ("eth", 0,
+        Subsignal("rst_n", Pins("L20")),
+        Subsignal("int_n", Pins("N30")),
+        Subsignal("mdio", Pins("J21")),
+        Subsignal("mdc", Pins("R23")),
+        Subsignal("rx_dv", Pins("R28")),
+        Subsignal("rx_er", Pins("V26")),
+        Subsignal("rx_data", Pins("U30 U25 T25 U28 R19 T27 T26 T28")),
+        Subsignal("tx_en", Pins("M27")),
+        Subsignal("tx_er", Pins("N29")),
+        Subsignal("tx_data", Pins("N27 N25 M29 L28 J26 K26 L30 J28")),
+        Subsignal("col", Pins("W19")),
+        Subsignal("crs", Pins("R30")),
+        IOStandard("LVCMOS25")
+    ),
+
+    ("ddram", 0,
+        Subsignal("a", Pins(
+            "AH12 AG13 AG12 AF12 AJ12 AJ13 AJ14 AH14",
+            "AK13 AK14 AF13 AE13 AJ11 AH11 AK10 AK11"),
+            IOStandard("SSTL15")),
+        Subsignal("ba", Pins("AH9 AG9 AK9"), IOStandard("SSTL15")),
+        Subsignal("ras_n", Pins("AD9"), IOStandard("SSTL15")),
+        Subsignal("cas_n", Pins("AC11"), IOStandard("SSTL15")),
+        Subsignal("we_n", Pins("AE9"), IOStandard("SSTL15")),
+        Subsignal("cs_n", Pins("AC12"), IOStandard("SSTL15")),
+        Subsignal("dm", Pins("Y16 AB17 AF17 AE16"),
+            IOStandard("SSTL15")),
+        Subsignal("dq", Pins(
+            "AA15 AA16 AC14 AD14 AA17 AB15 AE15 Y15",
+            "AB19 AD16 AC19 AD17 AA18 AB18 AE18 AD18",
+            "AG19 AK19 AG18 AF18 AH19 AJ19 AE19 AD19",
+            "AK16 AJ17 AG15 AF15 AH17 AG14 AH15 AK15"),
+            IOStandard("SSTL15_T_DCI")),
+        Subsignal("dqs_p", Pins("AC16 Y19 AJ18 AH16"),
+            IOStandard("DIFF_SSTL15")),
+        Subsignal("dqs_n", Pins("AC15 Y18 AK18 AJ16"),
+            IOStandard("DIFF_SSTL15")),
+        Subsignal("clk_p", Pins("AG10"), IOStandard("DIFF_SSTL15")),
+        Subsignal("clk_n", Pins("AH10"), IOStandard("DIFF_SSTL15")),
+        Subsignal("cke", Pins("AF10"), IOStandard("SSTL15")),
+        Subsignal("odt", Pins("AD8"), IOStandard("SSTL15")),
+        Subsignal("reset_n", Pins("AK3"), IOStandard("LVCMOS15")),
+        Misc("SLEW=FAST"),
+        Misc("VCCAUX_IO=HIGH")
+    ),
+]
 
 # Platform -----------------------------------------------------------------------------------------
 
@@ -71,7 +130,8 @@ class Platform(XilinxPlatform):
 class _CRG(Module):
     def __init__(self, platform, sys_clk_freq):
         self.clock_domains.cd_sys    = ClockDomain()
-        self.clock_domains.cd_clk125 = ClockDomain()
+        self.clock_domains.cd_sys4x  = ClockDomain(reset_less=True)
+        self.clock_domains.cd_clk200 = ClockDomain()
 
         # # #
 
@@ -79,47 +139,87 @@ class _CRG(Module):
         rst    = platform.request("cpu_reset")
         platform.add_period_constraint(clk200.p, 1e9/200e6)
 
-        self.submodules.pll = pll = S7PLL()
+        self.submodules.pll = pll = S7MMCM(speedgrade=-2)
         self.comb += pll.reset.eq(rst)
         pll.register_clkin(clk200, 200e6)
-        pll.create_clkout(self.cd_sys, sys_clk_freq)
-        pll.create_clkout(self.cd_clk125, 125e6)
+        pll.create_clkout(self.cd_sys,    sys_clk_freq)
+        pll.create_clkout(self.cd_sys4x,  4*sys_clk_freq)
+        pll.create_clkout(self.cd_clk200, 200e6)
 
+        self.submodules.idelayctrl = S7IDELAYCTRL(self.cd_clk200)
 
-class GTXTestSoC(SoCMini):
-    def __init__(self, platform, connector="pcie", linerate=2.5e9, use_pcie_refclk=True, with_loopback=True):
+# PCIe Analyzer ------------------------------------------------------------------------------------
+
+class PCIeAnalyzer(SoCSDRAM):
+    def __init__(self, platform, connector="pcie", linerate=2.5e9):
         assert connector in ["pcie"]
-        sys_clk_freq = int(100e6)
+        sys_clk_freq = int(150e6)
 
-        # SoCMini ----------------------------------------------------------------------------------
-        SoCMini.__init__(self, platform, sys_clk_freq)
+        # SoCSDRAM ----------------------------------------------------------------------------------
+        SoCSDRAM.__init__(self, platform, sys_clk_freq,
+            integrated_rom_size  = 0x8000,
+            integrated_sram_size = 0x1000,
+            uart_name            = "crossover",
+            csr_data_width       = 32,
+        )
 
         # CRG --------------------------------------------------------------------------------------
         self.submodules.crg = _CRG(platform, sys_clk_freq)
         platform.add_period_constraint(self.crg.cd_sys.clk, 1e9/100e6)
 
+        # DDR3 SDRAM -------------------------------------------------------------------------------
+        self.submodules.ddrphy = s7ddrphy.K7DDRPHY(platform.request("ddram"),
+            memtype      = "DDR3",
+            nphases      = 4,
+            sys_clk_freq = sys_clk_freq)
+        self.add_csr("ddrphy")
+        sdram_module = MT8JTF12864(sys_clk_freq, "1:4")
+        self.register_sdram(self.ddrphy,
+            geom_settings   = sdram_module.geom_settings,
+            timing_settings = sdram_module.timing_settings)
+
+        # Ethernet ---------------------------------------------------------------------------------
+        # phy
+        self.submodules.eth_phy = LiteEthPHY(
+            clock_pads = platform.request("eth_clocks"),
+            pads       = platform.request("eth"),
+            clk_freq   = sys_clk_freq)
+        self.add_csr("eth_phy")
+        # core
+        self.submodules.eth_core = LiteEthUDPIPCore(
+            phy         = self.eth_phy,
+            mac_address = 0x10e2d5000000,
+            ip_address  = "192.168.1.50",
+            clk_freq    = sys_clk_freq)
+        # etherbone
+        self.submodules.etherbone = LiteEthEtherbone(self.eth_core.udp, 1234)
+        self.add_wb_master(self.etherbone.wishbone.bus)
+
+        # timing constraints
+        self.platform.add_period_constraint(self.eth_phy.crg.cd_eth_rx.clk, 1e9/125e6)
+        self.platform.add_period_constraint(self.eth_phy.crg.cd_eth_tx.clk, 1e9/125e6)
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.eth_phy.crg.cd_eth_rx.clk,
+            self.eth_phy.crg.cd_eth_tx.clk)
+
         # GTX RefClk -------------------------------------------------------------------------------
-        if use_pcie_refclk:
-            refclk      = Signal()
-            refclk_freq = 100e6
-            refclk_pads = platform.request("pcie_refclk")
-            self.specials += Instance("IBUFDS_GTE2",
-                i_CEB   = 0,
-                i_I     = refclk_pads.p,
-                i_IB    = refclk_pads.n,
-                o_O     = refclk)
-        else:
-            refclk      = Signal()
-            refclk_freq = 125e6
-            self.comb += refclk.eq(ClockSignal("clk125"))
+        refclk      = Signal()
+        refclk_freq = 100e6
+        refclk_pads = platform.request("pcie_refclk")
+        self.specials += Instance("IBUFDS_GTE2",
+            i_CEB   = 0,
+            i_I     = refclk_pads.p,
+            i_IB    = refclk_pads.n,
+            o_O     = refclk)
 
         # GTX PLL ----------------------------------------------------------------------------------
         qpll = GTXChannelPLL(refclk, refclk_freq, linerate)
         print(qpll)
         self.submodules += qpll
 
+        # GTXs -------------------------------------------------------------------------------------
         for i in range(2):
-            # GTX --------------------------------------------------------------------------------------
             tx_pads = platform.request(connector + "_tx", i)
             rx_pads = platform.request(connector + "_rx", i)
             gtx = GTX(qpll, tx_pads, rx_pads, sys_clk_freq,
@@ -128,31 +228,12 @@ class GTXTestSoC(SoCMini):
                 tx_buffer_enable = True,
                 rx_buffer_enable = True)
             setattr(self.submodules, "gtx"+str(i), gtx)
-            if with_loopback:
-                self.comb += gtx.loopback.eq(0b010) # Near-End PMA Loopback
             platform.add_period_constraint(gtx.cd_tx.clk, 1e9/gtx.tx_clk_freq)
             platform.add_period_constraint(gtx.cd_rx.clk, 1e9/gtx.rx_clk_freq)
             self.platform.add_false_path_constraints(
                 self.crg.cd_sys.clk,
                 gtx.cd_tx.clk,
                 gtx.cd_rx.clk)
-
-            # Test -------------------------------------------------------------------------------------
-            counter = Signal(32)
-            sync_tx = getattr(self.sync, "gtx{}_tx".format(i))
-            sync_tx += counter.eq(counter + 1)
-
-            # K28.5 and slow counter --> TX
-            self.comb += [
-                gtx.encoder.k[0].eq(1),
-                gtx.encoder.d[0].eq((5 << 5) | 28),
-                gtx.encoder.k[1].eq(0),
-                gtx.encoder.d[1].eq(counter[26:]),
-            ]
-
-           # RX (slow counter) --> Leds
-            for j in range(2):
-                self.comb += platform.request("user_led", 2*i+j).eq(gtx.decoders[1].d[i])
 
 # Load ---------------------------------------------------------------------------------------------
 
@@ -168,7 +249,7 @@ def main():
     if "load" in sys.argv[1:]:
         load()
     platform = Platform()
-    soc     = GTXTestSoC(platform)
+    soc     = PCIeAnalyzer(platform)
     builder = Builder(soc, output_dir="build")
     builder.build(build_name="kc705")
 
