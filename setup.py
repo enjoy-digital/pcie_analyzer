2a1e0402aff99a6c298483e085009a47c66b0812
Florent Kermarrec <florent@enjoy-digital.fr>
2020-01-16 19:31:51 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+
+if sys.version_info[:3] < (3, 5):
+    raise SystemExit("You need Python 3.5+")
+
+
+setup(
+    name="pcie_analyzer",
+    version="0.1.dev",
+    description="Experiments with PCIe and FPGAs",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    author="Florent Kermarrec",
+    author_email="florent@enjoy-digital.fr",
+    url="http://enjoy-digital.fr",
+    download_url="https://github.com/enjoy-digital/pcie_analyzer",
+    test_suite="test",
+    license="BSD",
+    platforms=["Any"],
+    keywords="HDL ASIC FPGA hardware design",
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+        "Environment :: Console",
+        "Development Status :: Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
+    include_package_data=True,
+)
